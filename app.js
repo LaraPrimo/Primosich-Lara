8830a58f5501171c923cf9f5be7324762830a95f
LaraPrimo <lprimosich@gmail.com>
2021-08-20 15:40:15 -0300

    Lara-2

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require ("express");
-const app = express ();
-const path = require ("path");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.listen (3030, () => {
-    console.log ("Se prendió!");
+app.listen(3000, () => {
+  console.log("Se prendió!");
 });
 
-app.use (express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get ( "/", (req,res) => {
-    res.sendFile (path.join(__dirname, "views/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
 });
-app.get ( "/", (req,res) => {
-    res.sendFile (path.join(__dirname, "views/register.html"));
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
 });
